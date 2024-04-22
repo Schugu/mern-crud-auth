fb5a08074a5a35b5be7b07defbb5a03f63818543
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-04-22 13:23:46 -0300

    Backend Setup

--- src/app.js
+++ src/app.js
@@ -2,38 +2,18 @@
 // en las propiedades package.json (Esto significa que se puedan ocupar los modulos de ES6).
 
 // Importar express.
-import express from 'express'
+import express from 'express' 
+
+// Importar morgan
+import morgan from 'morgan'
 
 // Inicializar express y almacenar el objeto que nos devuelve en la constante app.
 const app = express();
 
-// App = Servidor 
-app.listen(3000);
-console.log('Server on port', 3000);
+// App utiliza el modulo morgan con su configuración dev. 
+app.use(morgan('dev'));
+
+export default app 
 
 
 
-
-
-
-
-
-
-
-// Carpetas
-//  Src 
-//    Routes : Nos servirá para crear las URL del BackEnd (definicion de endpoints o rutas
-//             que el FrontEnd pueda pedir).
-//    Controllers : Aquí se almacenarán las funciones que se van a ejecutar cuando visitemos
-//                  una URL.
-//    Models : Nos vaa a servir para guardar los modelos de datos de nuestra base de datos, 
-//             Aquí vamos a estar creando esquemas.  
-//    Middlewares : Nos va a servir para proteger rutas, para usuarios autentificados.  
-//    Schemas : Nos va a serivr para validar datos.
-//    Libs : Nos va a servir para escribir codigo que podamos importar varias veces.
-
-// Archivos
-//    app.js : Configuracion del código de express del BackEnd.
-//    db.js : Base de datos
-//    config.js : Nos va a servir para crear configuraciones que el resto de archivos va a pdoer importar.
-//    index.js : Aplicacion de arranque.
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import app from './app.js'
+
+// App = Servidor 
+app.listen(4000);
+console.log('Server on port', 4000);
